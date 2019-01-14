edb7f1ba11fc6c03603f7d42b40c447a52b8d5d8
akkumage <31241981+akkumage@users.noreply.github.com>
2019-01-14 12:01:55 +0530

    Add files via upload

---
+++ Resetpassword/Controller/Account/ForgotPasswordPost.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ *
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Pcnametag\Resetpassword\Controller\Account;
+
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Model\AccountManagement;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Escaper;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\SecurityViolationException;
+
+/**
+ * ForgotPasswordPost controller
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class ForgotPasswordPost extends \Magento\Customer\Controller\Account\ForgotPasswordPost
+{
+    /** @var AccountManagementInterface */
+    protected $customerAccountManagement;
+
+    /** @var Escaper */
+    protected $escaper;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     * @param AccountManagementInterface $customerAccountManagement
+     * @param Escaper $escaper
+     */
+    
+
+    /**
+     * Forgot customer password action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $email = (string)$this->getRequest()->getPost('email');
+        if ($email) {
+            if (!\Zend_Validate::is($email, 'EmailAddress')) {
+                $this->session->setForgottenEmail($email);
+                $this->messageManager->addErrorMessage(__('Please correct the email address.'));
+                return $resultRedirect->setPath('*/*/forgotpassword');
+            }
+
+            try {
+                $this->customerAccountManagement->initiatePasswordReset(
+                    $email,
+                    AccountManagement::EMAIL_RESET
+                );
+            } catch (NoSuchEntityException $exception) {
+                // Do nothing, we don't want anyone to use this action to determine which email accounts are registered.
+            } catch (SecurityViolationException $exception) {
+                $this->messageManager->addErrorMessage($exception->getMessage());
+                return $resultRedirect->setPath('*/*/forgotpassword');
+            } catch (\Exception $exception) {
+                $this->messageManager->addExceptionMessage(
+                    $exception,
+                    __('We could not find a web account associated with this email address. If you are an existing PC/Nametag customer, please create a new account with the same email address and your order history will be transferred.')
+                         );
+                return $resultRedirect->setPath('*/*/forgotpassword');
+            }
+            $this->messageManager->addSuccessMessage($this->getSuccessMessage($email));
+            return $resultRedirect->setPath('*/*/');
+        } else {
+            $this->messageManager->addErrorMessage(__('Please enter your email.'));
+            return $resultRedirect->setPath('*/*/forgotpassword');
+        }
+    }
+
+    /**
+     * Retrieve success message
+     *
+     * @param string $email
+     * @return \Magento\Framework\Phrase
+     */
+    protected function getSuccessMessage($email)
+    {
+        return __(
+            'Please check your inbox - you will receive an email at %1 with a link to reset your password',
+            $this->escaper->escapeHtml($email)
+        );
+    }
+}
---
+++ Resetpassword/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Pcnametag_Resetpassword',
+    __DIR__
+);
